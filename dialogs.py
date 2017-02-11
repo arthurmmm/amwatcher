aea40f22faba4822bfc9fde9f203a87a6e097fb1
arthurmmm <arthur_mao@live.com>
2017-02-11 11:35:29 +0800

    .

--- dialogs.py
+++ dialogs.py
@@ -28,6 +28,7 @@ def mongoCollection(cname):
 ROUTER = {
     'text': [
         ('^[\?\？]$', 'show_help'),
+        ('^[\?\？]{2}$', 'show_help_link'),
         ('^帮助$', 'show_help'),
         ('^[!！]\d*$', 'show_updates'),
         ('^[.。]$', 'show_follows'),
@@ -65,8 +66,10 @@ def _keyword_summary(keyword):
     text += '更新至：' 
     all_episodes = list(mongo_series.find({
         'keyword_id': keyword['_id'],
-    }, sort=[('episode', DESCENDING),('date_episode', DESCENDING),('first_upload_time', DESCENDING)]))
+    }, sort=[('season', DESCENDING),('episode', DESCENDING),('date_episode', DESCENDING),('first_upload_time', DESCENDING)]))
     latest_series = all_episodes[0]
+    if 'season' in latest_series and latest_series['season'] != 'NO_SEASON':
+        text += '%s ' % latest_series['season']
     if 'episode' in latest_series:
         text += '第%s话 ' % latest_series['episode']
     if 'date_episode' in latest_series:
@@ -111,7 +114,38 @@ def show_help(to_user):
     yield None
     msg_content, is_replay = yield None
     return ('TextMsg', HELP)
-        
+
+HELP_LINKS = ('NewsMsg', [
+    {
+        'title': '点剧菌使(tiao)用(xi)指南', 
+        'description': '', 
+        'url': 'http://mp.weixin.qq.com/s?__biz=MzI4NTYyNjc2OA==&mid=2247483666&idx=1&sn=1269b5abb4da27d94a61b97b6bc1b7a5&chksm=ebe819eddc9f90fb7937e90559797f850b09768bc867f1b50bc66c55ceb735c309357e2872ff#rd',
+        'pic_url': 'http://okmokavp8.bkt.clouddn.com/images/timg.jpg',
+    },
+    {
+        'title': '剧单 - 动画', 
+        'description': '', 
+        'url': 'http://www.jianshu.com/p/3dfbb46f957e',
+        'pic_url': 'http://okmokavp8.bkt.clouddn.com/20151004103746_yfhzC.jpeg',
+    },
+    {
+        'title': '剧单 - 日剧', 
+        'description': '', 
+        'url': 'http://www.jianshu.com/p/37d3efd2f073',
+        'pic_url': 'http://okmokavp8.bkt.clouddn.com/20151004103746_yfhzC.jpeg',
+    },
+    {
+        'title': '剧单 - 综艺', 
+        'description': '', 
+        'url': 'http://www.jianshu.com/p/d08be0e34e67',
+        'pic_url': 'http://okmokavp8.bkt.clouddn.com/20151004103746_yfhzC.jpeg',
+    },
+])
+def show_help_link(to_user):
+    yield None
+    msg_content, is_replay = yield None
+    return HELP_LINKS
+    
 def show_updates(to_user):
     yield None
     msg_content, is_replay = yield None
@@ -177,7 +211,7 @@ def show_updates(to_user):
             if new_feed_count > 0:
                 summary_text_list.append('%s[%s] 有%s个新资源' % (keyword['keyword'], TYPE_TXT[keyword['type']], new_feed_count))
     if not summary_text_list:
-        return ('TextMsg', '您关注的资源没有新的更新啦[上次查看时间：%s]' % last_check_time.strftime('%m/%d %H:%M'))
+        return ('TextMsg', '从上次查看[%s]到现在，关注的资源没有更新哦' % last_check_time.strftime('%Y/%m/%d %H:%M'))
     if back_days > 0:
         prefix = '--- %s天内的更新 ---' % back_days
     else:
@@ -190,7 +224,7 @@ def show_updates(to_user):
         end_suffix='--- 回复L显示详细列表 ---'
     ):
         selected, is_replay = yield msg
-        if selected in ['N', 'n']:
+        if selected in ['N', 'n'] and not is_last:
             continue
         elif selected in ['L', 'l']:
             for list_msg, is_last in _make_page(
@@ -199,7 +233,7 @@ def show_updates(to_user):
                 end_suffix='--- 结束 ---'
             ):
                 selected, is_replay = yield list_msg
-                if selected in ['N', 'n']:
+                if selected in ['N', 'n'] and not is_last:
                     continue
                 else:
                     raise UnexpectAnswer
@@ -245,11 +279,10 @@ def show_follows(to_user):
     text_list = ['%s [%s]' % (kw['keyword'], TYPE_TXT[kw['type']]) for kw in keywords]
     for msg, is_last in _make_page(
         text_list, 10, 
-        prefix='--- 关注资源列表 ---',
-        end_suffix='--- 回复!显示更新 ---'
+        prefix='--- 关注列表 ---',
     ):
         selected, is_replay = yield msg
-        if selected in ['N', 'n']:
+        if selected in ['N', 'n'] and not is_last:
             continue
         else:
             raise UnexpectAnswer
@@ -265,7 +298,7 @@ def _try_follow(to_user, already_followed, keyword):
                 'follow_keywords': keyword['_id'],
             }
         })
-        return ('TextMsg', '取关成功！')
+        return ('TextMsg', '%s取关成功!' % keyword['keyword'])
     else:
         mongo_users.find_one_and_update({
             'open_id': to_user
@@ -273,7 +306,7 @@ def _try_follow(to_user, already_followed, keyword):
         {
             '$addToSet': { 'follow_keywords': keyword['_id'] },
         }, upsert=True)
-        return ('TextMsg', '关注成功！')
+        return ('TextMsg', '%s关注成功!' % keyword['keyword'])
             
 def search_keyword(to_user):
     yield None # send none for start
@@ -283,6 +316,9 @@ def search_keyword(to_user):
     mongo_feeds = mongoCollection('feeds')
     mongo_series = mongoCollection('series')
     mongo_users = mongoCollection('users')
+    
+    user = mongo_users.find_one({'open_id': to_user})
+    
     try:
         keywords = mongo_keywords.find({
             '$or': [
@@ -299,27 +335,52 @@ def search_keyword(to_user):
             'status': 'activated',
         })
     except Exception:
-        return ('TextMsg', '∑(っ °Д °;)っ 请不要输入奇怪的东西啦 ')
+        return ('TextMsg', '∑(っ °Д °;)っ请不要输入奇怪的东西啦，真的会死机的哦~~')
     keywords = [k for k in keywords]
     count = len(keywords)
+    already_followed = {}
     if count == 0:
         return ('TextMsg', '没有找到"%s"相关的资源，如需添加新资源请发送邮件至: dianjutv@outlook.com' % msg_content.strip())
     elif count > 1:
         text_list = []
         num = 0
         for kw in keywords:
-            text_list.append('%s. %s [%s]' % (num, kw['keyword'], TYPE_TXT[kw['type']]))
+            if 'follow_keywords' in user and kw['_id'] in user['follow_keywords']:
+                text_line = '%s. %s [%s] [已关注]' % (num, kw['keyword'], TYPE_TXT[kw['type']])
+                already_followed[kw['_id']] = True
+            else:
+                text_line = '%s. %s [%s]' % (num, kw['keyword'], TYPE_TXT[kw['type']])
+                already_followed[kw['_id']] = False
+            text_list.append(text_line)
             num += 1
         for msg, is_last in _make_page(
             text_list, 10, 
-            prefix='--- 找到多个资源，请选择 ---', 
-            suffix='--- 回复数字选择，回复N翻页 ---', 
+            prefix='【回复F加数字(如F2)可以直接关注/取关资源哦】\n--- 找到了多个资源 ---', 
+            suffix='--- 回复N翻页 回复数字选择 ---', 
             end_suffix='--- 回复数字选择 ---'
         ):
             selected, is_replay = yield msg
-            if selected in ['N', 'n']:
+            
+            if selected in ['N', 'n'] and not is_last:
                 continue
             try:
+                while True:
+                    if selected[0] in ['F', 'f']:
+                        selected = int(selected[1:])
+                        keyword = keywords[selected]
+                        if not is_replay:
+                            msg_type, msg_content = _try_follow(to_user, already_followed[keyword['_id']], keyword)
+                            if '关注' in msg_content:
+                                clear_msg = re.sub('(%s.*?\]).*' % selected, lambda match: '%s [已关注]' % match.group(1) ,msg[1])
+                            else:
+                                clear_msg = re.sub('(%s.*?\]).*' % selected, lambda match: match.group(1) ,msg[1])
+                            msg_content = '%s\n(%s)' % (clear_msg, msg_content)
+                        else:
+                            msg_content = 'none'
+                        selected, is_replay = yield ('TextMsg', msg_content)
+                    else:
+                        logger.debug(selected)
+                        break
                 selected = int(selected)
                 if selected > count or selected < 0:
                     raise TypeError
@@ -333,59 +394,44 @@ def search_keyword(to_user):
     text, feeds, all_episodes, latest_series = _keyword_summary(keyword)
     
     text += '\n-----\n'
-    text += '回复L按剧集显示资源列表\n'
+    text += '回复L列出所有资源\n'
     # 查询用户是否关注了这个资源
-    user = mongo_users.find_one({'open_id': to_user})
     if 'follow_keywords' in user and keyword['_id'] in user['follow_keywords']:
-        already_followed = True
+        already_followed[keyword['_id']] = True
         text += '您已关注该资源，回复F取消关注'
     else:
-        already_followed = False
+        already_followed[keyword['_id']] = False
         text += '回复F关注该资源'
     selected, is_replay = yield ('TextMsg', text)
     
-    latest_series_index = 0
-    while True:
-        if selected in ['L', 'l']:
-            remain_list = all_episodes[latest_series_index:]
-            while True:
-                feeds = mongo_feeds.find({ '_id': {'$in': remain_list[0]['feeds']} })
-                text = '--- '
-                if 'episode' in remain_list[0]:
-                    text += 'EP%s ' % remain_list[0]['episode']
-                if 'date_episode' in remain_list[0]:
-                    text += '(%s) ' % remain_list[0]['date_episode']
-                text += '---'
-                for feed in feeds:
-                    text += '\n%(title)s\n%(href)s' % feed
-                latest_series_index += 1
-                remain_list = all_episodes[latest_series_index:]
-                if remain_list:
-                    if already_followed:
-                        text += '\n(回复N显示上一话，回复F取关)'
-                    else:
-                        text += '\n(回复N显示上一话，回复F关注)'
-                    selected, is_replay = yield ('TextMsg', text)
-                    if selected in ['n', 'N']:
-                        continue
-                    elif selected in ['f', 'F']:
-                        return _try_follow(to_user, already_followed, keyword)
-                    else:
-                        raise UnexpectAnswer
-                else:
-                    if already_followed:
-                        text += '\n--- 回复f取关 ---'
-                    else:
-                        text += '\n--- 回复F关注 ---'
-                    selected, is_replay = yield ('TextMsg', text)
-                    if selected in ['F', 'f']:
-                        return _try_follow(to_user, already_followed, keyword)
-                    else:
-                        raise UnexpectAnswer
-        elif selected in ['F', 'f']:
-            return _try_follow(to_user, already_followed, keyword)
-        else:
-            raise UnexpectAnswer
+    feeds = mongo_feeds.find({
+        'keyword_id': keyword['_id'],
+        'break_rules': {'$exists': False}
+    }).sort('upload_time', DESCENDING)
+    text_list = ['%(title)s\n%(href)s' % feed for feed in feeds]
+    if already_followed[keyword['_id']]:
+        suffix_follow = '回复F取关'
+    else:
+        suffix_follow = '回复F关注'
+    if selected in ['L', 'l']:
+        for msg, is_last in _make_page(
+            text_list, 5, 
+            suffix='--- 回复N翻页 %s ---' % suffix_follow, 
+            end_suffix='--- %s ---' % suffix_follow
+        ):
+            selected, is_replay = yield msg
+            
+            if selected in ['N', 'n'] and not is_last:
+                continue
+            elif selected in ['F', 'f']:
+                return _try_follow(to_user, already_followed[keyword['_id']], keyword)
+            else:
+                raise UnexpectAnswer
+    elif selected in ['F', 'f']:
+        return _try_follow(to_user, already_followed[keyword['_id']], keyword)
+    else:
+        raise UnexpectAnswer
+    
     return ('TextMsg', text)
             
 def active_user(to_user):
@@ -403,11 +449,12 @@ def active_user(to_user):
             'site': 'main',
         }
     }, upsert=True)
-    user_exists = res['updatedExisting']
-    if user_exists:
-        return ('TextMsg', '欢迎回来！')
-    else:
-        return ('TextMsg', '感谢关注！')
+    return HELP_LINKS
+    # user_exists = res['updatedExisting']
+    # if user_exists:
+        # return ('TextMsg', '欢迎回来！')
+    # else:
+        # return ('TextMsg', '感谢关注！')
         
 def deactive_user(to_user):
     yield None # send none for start
